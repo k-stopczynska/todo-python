1a415251ffa255eb099c1b1213685c61704b46ee
k.stopczynska <k.stopczynska@gmail.com>
2023-10-19 14:50:22 +0200

    added method to add new todo

---
+++ add_new_todo.py
@@ -0,0 +1,32 @@
+from connect import get_connection
+
+todo = {'title':'Learn Flask', 'description': 'Do Flask REST API to excercize',
+	'status': 'todo', 'deadline': '2023/12/21'
+}
+
+def add_new_todo(db_name, todo):
+	try:
+		cursor, db_connection = get_connection(db_name)
+		query = """INSERT INTO todo ({}) VALUES ('{}', '{}', '{}', '{}')""".format(
+            ', '.join(todo.keys()),
+           	 	todo['title'],
+            	todo['description'],
+            	todo['status'],
+            	todo['deadline'],
+        	)
+		cursor.execute(query)
+		db_connection.commit()
+		cursor.close()
+
+	except Exception as e:
+		print(e)
+
+	finally:
+		if db_connection:
+			db_connection.close()
+			print("Connection closed")
+
+
+add_new_todo('todos', todo)
+
+
--- create_table.py
+++ create_table.py
@@ -3,11 +3,17 @@ from connect import get_connection
 
 
 def create_table():
-	db_name = 'todos'
-	print('running')
-	cursor, db_connection = get_connection(db_name)
-	create_and_use_db(db_name)
-	query = """CREATE TABLE todo (todo_id INT PRIMARY KEY AUTO_INCREMENT, 
-	title VARCHAR(35) NOT NULL, description VARCHAR(255) NOT NULL, status 
-	ENUM('todo', 'pending', 'done'), deadline DATE NOT NULL)"""
-	cursor.execute(query)
+	try:
+		db_name = 'todos'
+		cursor, db_connection = get_connection(db_name)
+		create_and_use_db(db_name)
+		query = """CREATE TABLE todo (todo_id INT PRIMARY KEY AUTO_INCREMENT, 
+		title VARCHAR(35) NOT NULL, description VARCHAR(255) NOT NULL, status 
+		ENUM('todo', 'pending', 'done'), deadline DATE NOT NULL)"""
+		cursor.execute(query)
+	except Exception as e:
+		print(e)
+	finally:
+		if db_connection:
+			db_connection.close()
+			print("Connection closed")
