a120db7603e8e0d4f7c47a5ffa63e6c4a52d5322
k.stopczynska <k.stopczynska@gmail.com>
2023-10-19 15:12:55 +0200

    added query for getting todos by status

---
+++ get_todos_by_status.py
@@ -0,0 +1,24 @@
+from connect import get_connection
+
+
+def get_todos_by_status(db_name, table_name, status):
+	try:
+		cursor, db_connection = get_connection(db_name)
+		query = """SELECT * FROM {} as t WHERE t.status = '{}'""".format(table_name, status)
+		print(query)
+		cursor.execute(query)
+		results = cursor.fetchall()
+		for result in results:
+			print(result)
+		cursor.close()
+
+	except Exception as e:
+		print(e)
+
+	finally:
+		if db_connection:
+			db_connection.close()
+			print("Connection closed")
+
+
+get_todos_by_status('todos', 'todo', 'done')
