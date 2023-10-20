c3bbecd163a627eb38ebf4fb4ab287844c9aebf8
k.stopczynska <k.stopczynska@gmail.com>
2023-10-20 12:40:08 +0200

    added route for adding todos

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from db.create_table import create_table
 from model.get_all_todos import get_all_todos
 from model.get_todos_by_status import get_todos_by_status
 from model.get_todo_by_id import get_todo_by_id
+from model.add_new_todo import add_new_todo
 
 
 app = Flask(__name__)
@@ -25,6 +26,15 @@ def get_by_status(status):
 def get_by_status(todo_id):
     response = get_todo_by_id(db_name, table_name, todo_id)
     return jsonify(response)
+
+
+@app.route('/add_todo', method=['POST'])
+def add_todo():
+    new_todo = request.get_json()
+    add_new_todo(db_name, table_name, new_todo)
+    return new_todo
+
+
 
 
 
