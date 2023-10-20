c666d19e3f8f5dc2bc80ddd426b4df1a330805aa
k.stopczynska <k.stopczynska@gmail.com>
2023-10-20 19:07:33 +0200

    added functions to fetch and display todos

--- view/app.js
+++ view/app.js
@@ -4,16 +4,32 @@ const BASE_URL = 'http://127.0.0.1:5000/'
 const getAllTodos = async () => {
     try {
         const response = await fetch(BASE_URL)
-        console.log(response);
     if (!response.ok) {
         throw new Error('Could not fetch todos!');
     }
         const todos = await response.json();
-        console.log(todos);
+        return todos;
     } catch (err) {
         console.log(err);
     }
-    
 }
 
-getAllTodos()
+
+const createTodoElem = async () => {
+    const todos = await getAllTodos();
+    if (todos) {
+        for (const todo of todos) {
+            const todoElem = document.createElement('li')
+            todoElem.insertAdjacentHTML('afterbegin', `<h3>${todo['title']}</h3>
+            <p>${todo.description}</p>
+            <h4>${todo.status}</h4><span>${todo.deadline}</span>`)
+        todoList.append(todoElem)
+    }
+    }
+}
+
+createTodoElem()
+
+
+
+
